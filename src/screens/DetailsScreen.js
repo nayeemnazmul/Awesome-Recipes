220e457bee2df6110003654fa10ee4b8470e2620
nayeemnazmul <md.nazmul@northsouth.edu>
2019-05-25 13:26:36 +0600

    app navigator created

---
+++ src/screens/DetailsScreen.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+export default class DetailsScreen extends Component {
+  // state = {
+
+  // };
+
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Detail Screen</Text>
+      </View>
+    );
+  }
+}
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+export default class HomeScreen extends Component {
+  // state = {
+
+  // };
+
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
+}
