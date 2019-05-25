364c50e20d2e27a762d2e3942430fd651f0fed9e
nayeemnazmul <md.nazmul@northsouth.edu>
2019-05-25 20:46:47 +0600

    api call implemented

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,17 +2,28 @@ import React, { Component } from "react";
 import { View, Text } from "react-native";
 
 export default class HomeScreen extends Component {
-  // state = {
-
-  // };
-
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      data: null
+    };
+  }
+
+  componentWillMount() {
+    fetch(
+      "https://www.food2fork.com/api/search?key=7cbb796ab2a4ef5cb1a72c5f5eb4e113"
+    )
+      .then(response => response.json())
+      .then(responseJson => {
+        console.log("Response ", responseJson);
+      })
+      .catch(error => console.log("Error ", error));
   }
 
   render() {
     const { navigation } = this.props;
+
+    console.log("State ", this.state);
 
     return (
       <View>
