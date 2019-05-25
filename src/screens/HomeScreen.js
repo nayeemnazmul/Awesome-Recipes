43e0eadb19afed1b1d38512c57e5eaece27855ea
nayeemnazmul <md.nazmul@northsouth.edu>
2019-05-25 21:12:53 +0600

    loading indicator added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,29 +1,52 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
 
 export default class HomeScreen extends Component {
   constructor() {
     super();
     this.state = {
-      data: null
+      data: null,
+      loading: false
     };
   }
 
   componentWillMount() {
+    this.setState({
+      loading: true
+    });
+
     fetch(
       "https://www.food2fork.com/api/search?key=7cbb796ab2a4ef5cb1a72c5f5eb4e113"
     )
       .then(response => response.json())
       .then(responseJson => {
         console.log("Response ", responseJson);
+        this.setState({
+          data: responseJson,
+          loading: false
+        });
       })
-      .catch(error => console.log("Error ", error));
+      .catch(error => {
+        this.setState({
+          loading: false
+        });
+        console.log("Error ", error);
+      });
   }
 
   render() {
     const { navigation } = this.props;
+    const { loading } = this.state;
 
     console.log("State ", this.state);
+
+    if (loading) {
+      return (
+        <View style={styles.loadingIndicator}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      );
+    }
 
     return (
       <View>
@@ -32,3 +55,11 @@ export default class HomeScreen extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  loadingIndicator: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
